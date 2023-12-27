832d7aba5f3b668d78f8ab532e273d819e96e9c0
Tanner Bebb <17tbebb@gmail.com>
2023-12-27 12:19:23 -0800

    imported numpy and matplotlib. created a list on line 19 that has the 5 selected stocks. Created the stockArray variable that converts the stocks into an array.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
 import pprint
 
 def getClosing(ticker):
     stock = yf.Ticker(ticker)
-    hist = stock.history(period = "10d")
+    hist = stock.history(period="10d")
     closingList = []
     for price in hist['Close']:
-        closingList.append(price)
+        closingList.append(round(price, 2))
 
     return closingList
 
 
+'''
+Stocks list that has the 5 stocks selected
+'''
+stocks = ["MSFT", "AAPL", "GME", "SONY", "META"]
 
-msft = getClosing('MSFT')
-
-pprint.pprint(msft)
-
+stockArray = np.array(stocks)
